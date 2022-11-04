72858dc49c47b446c81e0a4bb9c25819970aea58
hussen87 <85638978+hussen87@users.noreply.github.com>
2022-11-04 16:33:32 +0300

    Update waad.py

--- jepthon/plugins/waad.py
+++ jepthon/plugins/waad.py
@@ -1,37 +1,37 @@
-from jepthon import jepiq as client
+from jepthon import jepiq 
 import telethon
 from core session import *
 from telethon import events
 import asyncio
-@client.on(events.NewMessage(outgoing=True, pattern=r"\.بخشيش وعد (.*)"))
+@jepiq.on(events.NewMessage(outgoing=True, pattern=r"\.بخشيش وعد (.*)"))
 async def _(event):
     for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
         chat = event.chat_id
-        await client.send_message(chat,'بخشيش')
+        await jepiq.send_message(chat,'بخشيش')
         await asyncio.sleep(605)
-@client.on(events.NewMessage(outgoing=True, pattern=r"\.راتب وعد (.*)"))
+@jepiq.on(events.NewMessage(outgoing=True, pattern=r"\.راتب وعد (.*)"))
 async def _(event):
     for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
         chat = event.chat_id
-        await client.send_message(chat,'راتب')
+        await jepiq.send_message(chat,'راتب')
         await asyncio.sleep(605)
         
-@client.on(events.NewMessage(outgoing=True, pattern=r"\.استثمار وعد (.*)"))
+@jepiq.on(events.NewMessage(outgoing=True, pattern=r"\.استثمار وعد (.*)"))
 async def _(event):
         for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
             
             chat = event.chat_id
-            await client.send_message(chat, 'فلوسي')
+            await jepiq.send_message(chat, 'فلوسي')
             await asyncio.sleep(0.5)
-            masg = await client.get_messages(chat, limit=1)
+            masg = await jepiq.get_messages(chat, limit=1)
             masg = masg[0].message
             masg = ("".join(masg.split(maxsplit=2)[2:])).split(" ", 2)
             msg = masg[0]
             if int(msg) > 500000000:
-                await client.send_message(chat, f"استثمار {msg}")
+                await jepiq.send_message(chat, f"استثمار {msg}")
                 await asyncio.sleep(10)
-                mssag2 = await client.get_messages(chat, limit=1)
+                mssag2 = await jepiq.get_messages(chat, limit=1)
                 await mssag2[0].click(text="اي ✅")
             else:
-                await client.send_message(chat, f"استثمار {msg}")
+                await jepiq.send_message(chat, f"استثمار {msg}")
             await asyncio.sleep(1210)
