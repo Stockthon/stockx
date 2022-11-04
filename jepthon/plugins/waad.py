19f15d765131d2bf99983afb2ce958585880432a
hussen87 <85638978+hussen87@users.noreply.github.com>
2022-11-04 16:22:31 +0300

    Update waad.py

--- jepthon/plugins/waad.py
+++ jepthon/plugins/waad.py
@@ -1,4 +1,4 @@
-from jepthon import jepiq as client
+from jepthon import jepiq as huks
 import telethon
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from core session import *
@@ -7,35 +7,35 @@ import asyncio
 
 
 
-@client.on(events.NewMessage(outgoing=True, pattern=r"\.بخشيش وعد (.*)"))
+@huks.on(events.NewMessage(outgoing=True, pattern=r"\.بخشيش وعد (.*)"))
 async def _(event):
     for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
         chat = event.chat_id
-        await client.send_message(chat,'بخشيش')
+        await huks.send_message(chat,'بخشيش')
         await asyncio.sleep(605)
-@client.on(events.NewMessage(outgoing=True, pattern=r"\.راتب وعد (.*)"))
+@huks.on(events.NewMessage(outgoing=True, pattern=r"\.راتب وعد (.*)"))
 async def _(event):
     for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
         chat = event.chat_id
-        await client.send_message(chat,'راتب')
+        await huks.send_message(chat,'راتب')
         await asyncio.sleep(605)
-        
-@client.on(events.NewMessage(outgoing=True, pattern=r"\.استثمار وعد (.*)"))
+       
+@huks.on(events.NewMessage(outgoing=True, pattern=r"\.استثمار وعد (.*)"))
 async def _(event):
         for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
             
             chat = event.chat_id
-            await client.send_message(chat, 'فلوسي')
+            await huks.send_message(chat, 'فلوسي')
             await asyncio.sleep(0.5)
-            masg = await client.get_messages(chat, limit=1)
+            masg = await huks.get_messages(chat, limit=1)
             masg = masg[0].message
             masg = ("".join(masg.split(maxsplit=2)[2:])).split(" ", 2)
             msg = masg[0]
             if int(msg) > 500000000:
-                await client.send_message(chat, f"استثمار {msg}")
+                await huks.send_message(chat, f"استثمار {msg}")
                 await asyncio.sleep(10)
-                mssag2 = await client.get_messages(chat, limit=1)
+                mssag2 = await huks.get_messages(chat, limit=1)
                 await mssag2[0].click(text="اي ✅")
             else:
-                await client.send_message(chat, f"استثمار {msg}")
+                await huks.send_message(chat, f"استثمار {msg}")
             await asyncio.sleep(1210)
