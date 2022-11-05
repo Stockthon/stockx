bc5eb2fb461e13f2741967d115d62018d08cd7a3
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-11-05 19:42:38 +0300

    Update waad.py

--- jepthon/plugins/waad.py
+++ jepthon/plugins/waad.py
@@ -5,13 +5,13 @@ import telethon
 async def _(event):
     for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
         chat = event.chat_id
-        await huks.send_message(chat,'بخشيش')
+        await jepiq.send_message(chat,'بخشيش')
         await asyncio.sleep(605)
 @jepiq.on(admin_cmd(pattern="\.ر وعد (.*)"))
 async def _(event):
     for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
         chat = event.chat_id
-        await huks.send_message(chat,'راتب')
+        await jepiq.send_message(chat,'راتب')
         await asyncio.sleep(605)
         
 @jepiq.on(admin_cmd(pattern="\.اس وعد (.*)"))
@@ -19,17 +19,17 @@ async def _(event):
         for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
             
             chat = event.chat_id
-            await huks.send_message(chat, 'فلوسي')
+            await jepiq.send_message(chat, 'فلوسي')
             await asyncio.sleep(0.5)
-            masg = await huks.get_messages(chat, limit=1)
+            masg = await jepiq.get_messages(chat, limit=1)
             masg = masg[0].message
             masg = ("".join(masg.split(maxsplit=2)[2:])).split(" ", 2)
             msg = masg[0]
             if int(msg) > 500000000:
-                await huks.send_message(chat, f"استثمار {msg}")
+                await jepiq.send_message(chat, f"استثمار {msg}")
                 await asyncio.sleep(10)
                 mssag2 = await huks.get_messages(chat, limit=1)
                 await mssag2[0].click(text="اي ✅")
             else:
-                await huks.send_message(chat, f"استثمار {msg}")
+                await jepiq.send_message(chat, f"استثمار {msg}")
             await asyncio.sleep(1210)
