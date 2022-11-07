41309857dbccfe482bf14b635ccaef0f8d53669e
hussen87 <85638978+hussen87@users.noreply.github.com>
2022-11-07 22:12:19 +0300

    Update cgo.py

--- jepthon/plugins/cgo.py
+++ jepthon/plugins/cgo.py
@@ -1,4 +1,34 @@
 from ..core.session import jepiq
-@jepiq.on(admin_cmd(pattern=r"\.وعد ر (.*)"))
-async def _(event):    
-    await event.edit("{كسج حلو}")
+import asyncio
+@jepiq.on(events.NewMessage(outgoing=True, pattern=r"\وعد بخ (.*)"))
+async def _(event):
+    for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
+        chat = event.chat_id
+        await jepiq.send_message(chat,'بخشيش')
+        await asyncio.sleep(605)
+@jepiq.on(events.NewMessage(outgoing=True, pattern=r"\وعد ر (.*)"))
+async def _(event):
+    for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
+        chat = event.chat_id
+        await jepiq.send_message(chat,'راتب')
+        await asyncio.sleep(605)
+        
+@jepiq.on(events.NewMessage(outgoing=True, pattern=r"\وعد اس (.*)"))
+async def _(event):
+        for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
+            
+            chat = event.chat_id
+            await jepiq.send_message(chat, 'فلوسي')
+            await asyncio.sleep(0.5)
+            masg = await jepiq.get_messages(chat, limit=1)
+            masg = masg[0].message
+            masg = ("".join(masg.split(maxsplit=2)[2:])).split(" ", 2)
+            msg = masg[0]
+            if int(msg) > 500000000:
+                await jepiq.send_message(chat, f"استثمار {msg}")
+                await asyncio.sleep(10)
+                mssag2 = await jepiq.get_messages(chat, limit=1)
+                await mssag2[0].click(text="اي ✅")
+            else:
+                await jepiq.send_message(chat, f"استثمار {msg}")
+            await asyncio.sleep(1210)
