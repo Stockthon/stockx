92ca3baf260f451223b98ddf13a1f8661ee95506
hussen87 <85638978+hussen87@users.noreply.github.com>
2022-11-07 22:04:13 +0300

    Update cgo.py

--- jepthon/plugins/cgo.py
+++ jepthon/plugins/cgo.py
@@ -1,4 +1,4 @@
 from ..core.session import jepiq
-@jepiq.on(admin_cmd(pattern="فحصص(?: |$)(.*)"))
+@jepiq.on(admin_cmd(pattern=r"\.وعد ر (.*)"))
 async def _(event):    
     await event.edit("{كسج حلو}")
