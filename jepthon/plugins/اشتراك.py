605b1c344d832e66db4b1226f2f9c0bbc0dc9d2b
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-11-15 15:10:37 +0300

    Update اشتراك.py

--- jepthon/plugins/اشتراك.py
+++ jepthon/plugins/اشتراك.py
@@ -3,9 +3,10 @@ from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 import asyncio
 from ..core.managers import edit_delete, edit_or_reply
 
-@jepiq.ar_cmd(pattern="اشتراك.")
+@jepiq.ar_cmd(pattern="اشتراك")
 async def reda(event):
-    ty = event.text.replace("اشتراك.", "")
+    ty = event.text
+    ty = ty.replace(".اشتراك", "")
     ty = ty.replace(" ", "")
     if len (ty) < 2:
         return await edit_delete(event, "**قم بكتابة نوع الاشتراك الاجباري كروب او خاص**")
@@ -25,9 +26,31 @@ async def reda(event):
         if not gvarstatus ("subprivate"):
             addgvar ("subprivate", True)
             await edit_or_reply(event, "**تم تفعيل الاشتراك الاجباري للخاص**")
- 
+    if ty not in ["خاص", "كروب"]:
+        return await edit_delete(event, "**قم بكتابة نوع الاشتراك الاجباري كروب او خاص**")
+@jepiq.ar_cmd(pattern="ايقاف")
+async def reda (event):
+    cc = event.text.replace(".الغاء", "")
+    cc = cc.replace(" ", "")
+    if len (cc) < 2:
+        return await edit_delete(event, "**قم بكتابة نوع الاشتراك الاجباري لإلغائه**")
+    if cc == "كروب":
+        if not gvarstatus ("subgroup"):
+            return await edit_delete("**لم تفعل الاشتراك الاجباري للكروب لإلغائه**")
+        if gvarstatus ("subgroup"):
+            delgvar ("subgroup")
+            return await edit_delete(event, "**تم الغاء الاشتراك الاجباري للكروب بنجاح**")
+    if cc == "خاص":
+        if not gvarstatus ("subprivate"):
+            return await edit_delete(event, "** الاشتراك الاجباري للخاص غير مفعل لإلغائه **")
+        if gvarstatus ("subprivate"):
+            delgvar ("subprivate")
+            return await edit_delete(event, "**تم إلغاء الاشتراك الاجباري للخاص**")
+    if cc not in ["خاص", "كروب"]:
+        return await edit_delete(event, "**قم بكتابة نوع الاشتراك الاجباري لإلغائه**")
+
 @jepiq.ar_cmd(incoming=True)
 async def reda(event):
     if gvarstatus ("subprivate"):
         if event.is_private:
-            await edit_or_reply(event, str(event))
+            await jepiq.send_message(event.chat_id, "**شبيك يغبي**")
